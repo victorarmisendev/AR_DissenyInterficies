9599112669a792cdca116398cced2618d890134e
Trurmix <lazarogame.evdev@gmail,com>
2018-10-23 20:13:58 +0200

    First Commid All
    Aumentada realidad

---
+++ Interficies/Assets/Die.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+  
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetTouch(0).position.x > 0.0f)
+        {
+            GetComponent<Animator>().SetTrigger("Die");
+        }
+        else
+        {
+            GetComponent<Animator>().SetTrigger("Attack");
+        }
+    }
+}
